42bb2b91ce71af6f2efd43af375ce4636195106c
kolariki <ivankolariki1990@gmail.com>
2022-07-08 15:15:21 -0300

    cambios

---
+++ listado_productos.php
@@ -0,0 +1,48 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$aProductos = array();
+
+$aProductos[] = array ("nombre" => "Smart TV 55\" 4K UHD",
+"marca" => "Hitachi",
+"modelo" => "554KS20",
+"stock" => 60,
+"precio" => 58000,
+); 
+
+$aProductos[] = array ("nombre" => "Samsung Galaxy A30 Blanco",
+"marca" => "Samsung",
+"modelo" => "Galaxy A30",
+"stock" => 0,
+"precio" => 22000,
+); 
+
+$aProductos[] = array ("nombre" => "Aire Acondicionado Split Inverter Frio/Calor Surrey 2900F",
+"marca" => "Surrey",
+"modelo" => "553AIQ1201E",
+"stock" => 5,
+"precio" => 45000,
+); 
+
+
+//print_r($aProductos);
+?>
+
+
+
+<html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Listado</title>
+</head>
+<body>
+    
+</body>
+</html>
--- rand.php
+++ rand.php
@@ -10,7 +10,9 @@
 
 <body>
     <?php
+    
     $valor = rand(1, 5);
+
     if ($valor == 1  || $valor == 3 || $valor == 5) {
         echo "el numero $valor es impar";
     } else {
